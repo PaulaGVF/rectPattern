4f545cbdeb24b4769d049393e5d7512c5a400635
Paula Galindo <paulagalindovf@live.com>
2015-11-01 21:19:36 -0200

    Update rect.c

--- rect.c
+++ rect.c
@@ -1,20 +1,36 @@
 #include<stdio.h>
- 
+#include <string.h>
+/************************************** 
+* Trabalho 2 de Programacao de Computadores I - Retangulo
+* Curso de Sistemas da informacao
+* Aluno: Paula Galindo - matricula: 0050013381
+* Professor: Alex Salgado
+***************************************/ 
 int main() {
-   int i, a,b,j;
+   int i,a,b,j;
+  char opcao; 
+  do{
    printf("Entre com a altura\n");
    scanf("%d",&b);
     printf("Entre com a largura\n");
    scanf("%d",&a);
+  
+   printf("Olá, meu nome e Paula e o retangulo ficou assim:\n");
    for (i = 0; i < b; i++) {
       printf("\n");
       for (j = 0; j < a; j++) {
          if (i == 0 || i == (b-1) || j == 0 || j == (a-1))
-            printf("?");
+           printf("*");
          else
             printf(" ");
-      }
-   }
-   return (0);
+            
+            
+
+ }
+} 
+printf("Deseja continuar(s/n)\n");
+   scanf(" %c",&opcao); 
+  }while(opcao == 's');  	
+   return 'n';
 }
 
